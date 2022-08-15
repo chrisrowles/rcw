2b1cb5fe75153f38f7463a5a30c867278e5f45f9
c6i <crowles.gd@gmail.com>
2022-08-15 12:39:09 +0100

    separate ui and logic

---
+++ RacingWheelTracker/ControllerManager.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Gaming.Input;
+using Windows.UI.Core;
+
+
+namespace RacingWheelTracker
+{
+    public class ControllerManager
+    {
+        /// <summary>
+        /// List to contain detected controllers.
+        /// </summary>
+        public List<RawGameController> controllers = new List<RawGameController>();
+
+        /// <summary>
+        /// The active controller.
+        /// </summary>
+        public RawGameController activeController;
+
+        /// <summary>
+        /// Handler for detecting controller connection.
+        /// </summary>
+        private static event EventHandler<RawGameController> ControllerAdded;
+
+        /// <summary>
+        /// Handler for detecting controller disconnection.
+        /// </summary>
+        private static event EventHandler<RawGameController> ControllerRemoved;
+
+        /// <summary>
+        /// Timestamp of the active controller's current reading.
+        /// </summary>
+        public ulong currentReading;
+
+        /// <summary>
+        /// Array to map controller button states.
+        /// i.e. {'BUTTON_0': false, 'BUTTON_1': false}...
+        /// </summary>
+        public bool[] controllerButtonMap;
+
+        /// <summary>
+        /// Array to map controller axis state
+        /// i.e. {0.0, 0.0, 0.0, 0.0}
+        /// </summary>
+        public double[] controllerAxisMap;
+
+        /// <summary>
+        /// Active contoller x-axis value.
+        /// </summary>
+        public double xAxisCurrentValue;
+
+        /// <summary>
+        /// Active controller switch position mapping.
+        /// i.e. {'Center': 0, 'Down': 5}
+        /// </summary>
+        public GameControllerSwitchPosition[] controllerSwitchMap;
+
+        /// <summary>
+        /// Main page
+        /// TODO better way
+        /// </summary>
+        public MainPage mainPage;
+
+        /// <summary>
+        /// Flag to determine whether or not the polling
+        /// task is running for the active controller.
+        /// </summary>
+        public bool taskRunning = true;
+
+        /// <summary>
+        /// Lock for looping through controllers.
+        /// A lock is required due to the possibility of controller state changes
+        /// i.e. connects and disconnects.
+        /// </summary>
+        private readonly object refLock = new object();
+
+        public ControllerManager(MainPage mainPage)
+        {
+            this.mainPage = mainPage;
+        }
+
+        /// <summary>
+        /// Handle controller connect event.
+        /// </summary>
+        /// <param name="controller"></param>
+        public void OnControllerAdded(RawGameController controller)
+        {
+            EventHandler<RawGameController> handler = ControllerAdded;
+            handler?.Invoke(this, controller);
+
+            bool controllerInList = controllers.Contains(controller);
+
+            if (!controllerInList)
+            {
+                controllers.Add(controller);
+            }
+        }
+
+        /// <summary>
+        /// Handle controller disconnect event.
+        /// </summary>
+        /// <param name="controller"></param>
+        public void OnControllerRemoved(RawGameController controller)
+        {
+            EventHandler<RawGameController> handler = ControllerRemoved;
+            handler?.Invoke(this, controller);
+
+            bool controllerInList = controllers.Contains(controller);
+
+            if (!controllerInList)
+            {
+                controllers.Remove(controller);
+            }
+        }
+
+        /// <summary>
+        /// Initialise the active controller.
+        /// This method begins by detecting all connected game controllers.
+        /// It then sets the activeController and updates the UI controller
+        /// name field.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void InitActiveController()
+        {
+            DetectConnectedControllers();
+
+            Uri uri = new Uri("http://127.0.0.1:8086");
+            mainPage.controllerTrackingWebview.Navigate(uri);
+
+            if (controllers.Count > 0)
+            {
+                activeController = controllers.First();
+                mainPage.controllerDisplayName = activeController.DisplayName;
+            }
+            else
+            {
+                mainPage.controllerDisplayName = "No controller detected.";
+            }
+        }
+
+        /// <summary>
+        /// Initialise active controller tracking.
+        /// This method begins by setting the values of the active
+        /// controller mapping properties. It then dispatches the
+        /// polling task.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void InitControllerTracking()
+        {
+            controllerButtonMap = new bool[activeController.ButtonCount];
+            controllerAxisMap = new double[activeController.AxisCount];
+            controllerSwitchMap = new GameControllerSwitchPosition[activeController.ButtonCount];
+
+            SetActiveControllerMaps();
+
+            Task.Run(() => TrackActiveController());
+        }
+
+        /// <summary>
+        /// Active controller tracking task.
+        /// This method first checks to see if the task is running.
+        /// If it is, then it fetches the current reading of the
+        /// active controller and updates the controller mapping
+        /// properties accordingly.
+        /// </summary>
+        /// <returns></returns>
+        private async Task TrackActiveController()
+        {
+            while (true)
+            {
+                if (taskRunning)
+                {
+                    await mainPage.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+                        currentReading = activeController.GetCurrentReading(controllerButtonMap, controllerSwitchMap, controllerAxisMap);
+
+                        mainPage.activeReadingIdField = currentReading.ToString();
+                        SetActiveControllerMaps();
+                    });
+                }
+
+                await Task.Delay(50);
+            }
+        }
+
+        /// <summary>
+        /// Detect connected controllers.
+        /// Loops through the RawGameControllers object and
+        /// adds connected controllers to the list.
+        /// </summary>
+        private void DetectConnectedControllers()
+        {
+            lock (refLock)
+            {
+                foreach (var controller in RawGameController.RawGameControllers)
+                {
+                    bool controllerInList = controllers.Contains(controller);
+
+                    if (!controllerInList)
+                    {
+                        controllers.Add(controller);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set the active controller's mapping properties.
+        /// </summary>
+        private async void SetActiveControllerMaps()
+        {
+            var xAxisNextValue = controllerAxisMap.ElementAt(0);
+
+            mainPage.controllerCurrentThrottleValueField = Math.Round(controllerAxisMap.ElementAt(2), 4).ToString();
+            mainPage.controllerCurrentBrakeValueField = Math.Round(controllerAxisMap.ElementAt(3), 4).ToString();
+            mainPage.controllerCurrentXAxisValueField = Math.Round(xAxisNextValue - 0.5022, 4).ToString();
+
+            if (xAxisNextValue != xAxisCurrentValue)
+            {
+                mainPage.controllerCurrentXAxisDirectionField = xAxisNextValue > xAxisCurrentValue
+                    ? "Right"
+                    : "Left";
+            }
+
+            xAxisCurrentValue = xAxisNextValue;
+
+            string dispatchAxisUpdateEvent = String.Format("const event = new CustomEvent('axis-update', {{ detail: {0} }}); document.dispatchEvent(event);", xAxisCurrentValue);
+            await mainPage.controllerTrackingWebview.InvokeScriptAsync("eval", new string[] { dispatchAxisUpdateEvent });
+        }
+    }
+}
--- RacingWheelTracker/MainPage.xaml.cs
+++ RacingWheelTracker/MainPage.xaml.cs
@@ -13,66 +13,87 @@ namespace RacingWheelTracker
 {
     public sealed partial class MainPage : Page
     {
-        /// <summary>
-        /// Lock for looping through controllers.
-        /// A lock is required due to the possibility of controller state changes
-        /// i.e. connects and disconnects.
-        /// </summary>
-        private readonly object refLock = new object();
+        public string controllerDisplayName
+        {
+            get
+            {
+                return ControllerDisplayLabel.Text;
+            }
+            set
+            {
+                ControllerDisplayLabel.Text = value;
+            }
+        }
 
-        /// <summary>
-        /// List to contain detected controllers.
-        /// </summary>
-        private List<RawGameController> controllers = new List<RawGameController>();
+        public WebView controllerTrackingWebview
+        {
+            get
+            {
+                return controllerTrackingFrame;
+            }
+        }
 
-        /// <summary>
-        /// The active controller.
-        /// </summary>
-        private RawGameController activeController;
+        public string controllerCurrentThrottleValueField
+        {
+            get
+            {
+                return CurrentThrottle.Text;
+            }
+            set
+            {
+                CurrentThrottle.Text = value;
+            }
+        }
 
-        /// <summary>
-        /// Handler for detecting controller connection.
-        /// </summary>
-        private static event EventHandler<RawGameController> ControllerAdded;
+        public string controllerCurrentBrakeValueField
+        {
+            get
+            {
+                return CurrentBrake.Text;
+            }
+            set
+            {
+                CurrentBrake.Text = value;
+            }
+        }
 
-        /// <summary>
-        /// Handler for detecting controller disconnection.
-        /// </summary>
-        private static event EventHandler<RawGameController> ControllerRemoved;
+        public string controllerCurrentXAxisValueField
+        {
+            get
+            {
+                return CurrentXAxis.Text;
+            }
+            set
+            {
+                CurrentXAxis.Text = value;
+            }
+        }
 
-        /// <summary>
-        /// Timestamp of the active controller's current reading.
-        /// </summary>
-        private ulong currentReading;
+        public string controllerCurrentXAxisDirectionField
+        {
+            get
+            {
+                return CurrentXAxisDirection.Text;
+            }
+            set
+            {
+                CurrentXAxisDirection.Text = value;
+            }
+        }
 
-        /// <summary>
-        /// Array to map controller button states.
-        /// i.e. {'BUTTON_0': false, 'BUTTON_1': false}...
-        /// </summary>
-        private bool[] controllerButtonMap;
+        public string activeReadingIdField
+        {
+            get
+            {
+                return ActiveReadingId.Text;
+            }
+            set
+            {
+                ActiveReadingId.Text = value;
+            }
+        }
 
-        /// <summary>
-        /// Array to map controller axis state
-        /// i.e. {0.0, 0.0, 0.0, 0.0}
-        /// </summary>
-        private double[] controllerAxisMap;
-
-        /// <summary>
-        /// Active contoller x-axis value.
-        /// </summary>
-        private double xAxisCurrentValue;
-
-        /// <summary>
-        /// Active controller switch position mapping.
-        /// i.e. {'Center': 0, 'Down': 5}
-        /// </summary>
-        private GameControllerSwitchPosition[] controllerSwitchMap;
-
-        /// <summary>
-        /// Flag to determine whether or not the polling
-        /// task is running for the active controller.
-        /// </summary>
-        private bool taskRunning = true;
+        public ControllerManager controllerManager; 
 
         public MainPage()
         {
@@ -80,176 +101,28 @@ namespace RacingWheelTracker
 
             ApplicationView.PreferredLaunchViewSize = new Size(750, 650);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            controllerManager = new ControllerManager(this);
         }
 
-        /// <summary>
-        /// Handle controller connect event.
-        /// </summary>
-        /// <param name="controller"></param>
-        public void OnControllerAdded(RawGameController controller)
+        private void ConnectButtonClick(object sender, RoutedEventArgs e)
         {
-            EventHandler<RawGameController> handler = ControllerAdded;
-            handler?.Invoke(this, controller);
-
-            bool controllerInList = controllers.Contains(controller);
-
-            if (!controllerInList)
-            {
-                controllers.Add(controller);
-            }
+            controllerManager.InitActiveController();
+        }
+        
+        private void InitTrackingButtonClick(object sender, RoutedEventArgs e)
+        {
+            controllerManager.InitControllerTracking();
         }
 
-        /// <summary>
-        /// Handle controller disconnect event.
-        /// </summary>
-        /// <param name="controller"></param>
-        public void OnControllerRemoved(RawGameController controller)
+        private void StartControllerTrackingButtonClick(object sender, RoutedEventArgs e)
         {
-            EventHandler<RawGameController> handler = ControllerRemoved;
-            handler?.Invoke(this, controller);
-
-            bool controllerInList = controllers.Contains(controller);
-
-            if (!controllerInList)
-            {
-                controllers.Remove(controller);
-            }
+            controllerManager.taskRunning = true; 
         }
 
-        /// <summary>
-        /// Initialise the active controller.
-        /// This method begins by detecting all connected game controllers.
-        /// It then sets the activeController and updates the UI controller
-        /// name field.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void InitActiveController(object sender, RoutedEventArgs e)
+        private void StopControllerTrackingButtonClick(object sender, RoutedEventArgs e)
         {
-            DetectConnectedControllers();
-
-            Uri uri = new Uri("http://127.0.0.1:8086");
-            WebViewM.Navigate(uri);
-
-            if (controllers.Count > 0)
-            {
-                activeController = controllers.First();
-                ControllerDisplayName.Text = activeController.DisplayName;
-            }
-            else
-            {
-                ControllerDisplayName.Text = "No controller detected.";
-            }
-        }
-
-        /// <summary>
-        /// Initialise active controller tracking.
-        /// This method begins by setting the values of the active
-        /// controller mapping properties. It then dispatches the
-        /// polling task.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void InitControllerTracking(object sender, RoutedEventArgs e)
-        {
-            controllerButtonMap = new bool[activeController.ButtonCount];
-            controllerAxisMap = new double[activeController.AxisCount];
-            controllerSwitchMap = new GameControllerSwitchPosition[activeController.ButtonCount];
-
-            SetActiveControllerMaps();
-
-            Task.Run(() => TrackActiveController());
-        }
-
-        /// <summary>
-        /// Active controller tracking task.
-        /// This method first checks to see if the task is running.
-        /// If it is, then it fetches the current reading of the
-        /// active controller and updates the controller mapping
-        /// properties accordingly.
-        /// </summary>
-        /// <returns></returns>
-        private async Task TrackActiveController()
-        {
-            while (true)
-            {
-                if (taskRunning)
-                {
-                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
-                        currentReading = activeController.GetCurrentReading(controllerButtonMap, controllerSwitchMap, controllerAxisMap);
-
-                        ActiveReadingId.Text = currentReading.ToString();
-                        SetActiveControllerMaps();
-                    });
-                }
-
-                await Task.Delay(50);
-            }
-        }
-
-        /// <summary>
-        /// Detect connected controllers.
-        /// Loops through the RawGameControllers object and
-        /// adds connected controllers to the list.
-        /// </summary>
-        private void DetectConnectedControllers()
-        {
-            lock (refLock)
-            {
-                foreach (var controller in RawGameController.RawGameControllers)
-                {
-                    bool controllerInList = controllers.Contains(controller);
-
-                    if (!controllerInList)
-                    {
-                        controllers.Add(controller);
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Set the task to true to start running it.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void StartControllerTracking(object sender, RoutedEventArgs e)
-        {
-            taskRunning = true;
-        }
-
-        /// <summary>
-        /// Set the task to true to stop running it.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void StopControllerTracking(object sender, RoutedEventArgs e)
-        {
-            taskRunning = false;
-        }
-
-        /// <summary>
-        /// Set the active controller's mapping properties.
-        /// </summary>
-        private async void SetActiveControllerMaps()
-        {
-            var xAxisNextValue = controllerAxisMap.ElementAt(0);
-
-            CurrentThrottle.Text = Math.Round(controllerAxisMap.ElementAt(2), 4).ToString();
-            CurrentBrake.Text = Math.Round(controllerAxisMap.ElementAt(3), 4).ToString();
-            CurrentXAxis.Text = Math.Round(xAxisNextValue - 0.5022, 4).ToString();
-
-            if (xAxisNextValue != xAxisCurrentValue)
-            {
-                CurrentXAxisDirection.Text = xAxisNextValue > xAxisCurrentValue
-                    ? "Right"
-                    : "Left";
-            }
-
-            xAxisCurrentValue = xAxisNextValue;
-
-            string dispatchAxisUpdateEvent = String.Format("const event = new CustomEvent('axis-update', {{ detail: {0} }}); document.dispatchEvent(event);", xAxisCurrentValue);
-            await WebViewM.InvokeScriptAsync("eval", new string[] { dispatchAxisUpdateEvent });
+            controllerManager.taskRunning = false;
         }
     }
 }
